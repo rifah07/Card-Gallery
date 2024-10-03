43aea6aa638fbdebf26b139b6823d56571eec63b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-03 17:12:09 +0600

    Project done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*,{html.js.jsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand:{
+          light: '#7f9cf5',
+          DEFAULT: '#3f51b5',
+          dark: '#1c3a73',
+        }
+      }
+    },
   },
   plugins: [],
 }
